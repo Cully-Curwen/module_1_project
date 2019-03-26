f9a3642e995c16d26f34d51c18c15618b210ad37
github username <github email address>
2019-03-26 10:33:35 +0000

    Test.produce 1st draft

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,10 @@
 class Test < ActiveRecord::Base
+
+  def self.produce(user)
+    session_new = Test.maximum(session) + 1 
+    rand_id = rand(Question.maximum(id))
+    Test.create(user_id: user.id, question_id: rand_id, session: session_new, credibility: null)
+  end
+
 end
     
