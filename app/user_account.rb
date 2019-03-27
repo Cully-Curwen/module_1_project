357e3a98bf80404f56db9ba4640f87b8abcdd525
Cully-Curwen <github email address>
2019-03-27 12:39:09 +0000

    tied in user_account so exits through landingpage

--- app/user_account.rb
+++ app/user_account.rb
@@ -1,21 +1,21 @@
 # require_relative '../config/environment'
 class UserAccount
 
-@prompt = TTY::Prompt.new
+  @prompt = TTY::Prompt.new
 
-def self.edit_user 
+  def self.edit_user 
     new_choices = ["Update Username", "Update Password", "Delete Account", "Go Back"]
-   case @prompt.select("What would you like to do", new_choices)
-   when "Update Username"
-    User.update_username
-   when "Update Password"
-    User.update_password
-   when "Delete Account"
-    User.delete_account
-    Welcome.new_page
-   when "Go Back"
-    #either main menu page here
-   end
-end
+    case @prompt.select("What would you like to do", new_choices)
+    when "Update Username"
+      User.update_username
+    when "Update Password"
+      User.update_password
+    when "Delete Account"
+      User.delete_account
+      live = false
+    when "Go Back"
+      true
+    end
+  end
 
 end
