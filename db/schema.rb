425f9491c3db0ae9ff16b158418660fd0f39a43a
Cully-Curwen <github email address>
2019-03-27 17:25:44 +0000

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_133459) do
+ActiveRecord::Schema.define(version: 2019_03_27_172026) do
 
   create_table "questions", force: :cascade do |t|
     t.string "category"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2019_03_26_133459) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password"
+    t.integer "high_score"
+    t.string "category"
   end
 
 end
